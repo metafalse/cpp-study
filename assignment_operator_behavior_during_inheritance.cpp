b00a10186d4fd47bbba550e4333831632cfd6416
metaphallus <weatherbreak@wine.ocn.ne.jp>
2016-06-25 00:32:21 -0700

    Create assignment_operator_behavior_during_inheritance.cpp

---
+++ assignment_operator_behavior_during_inheritance.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class CBase {
+    public:
+        CBase (const char* sName);
+        const CBase &operator=(const char* sName);
+        void Show() { cout << m_aName << "\n"; }
+    protected:
+        char m_aName[20];
+};
+
+CBase::CBase(const char* sName)
+{
+    cout << "CBase Constructor\n";
+    strcpy(m_aName, sName);
+}
+
+const CBase &CBase::operator=(const char* sName)
+{
+    cout << "CBase Operator function\n";
+    strcpy(m_aName, sName);
+    return *this;
+}
+
+class CDerived : public CBase {
+    public:
+        CDerived(const char* sName) : CBase(sName)
+        {
+            cout << "CDerived Constructor\n";
+        }
+};
+
+int main() {
+    CDerived ObjD1("Suleman");
+    ObjD1.Show();
+    cout << "\n\n";
+    cout << "Call to String Assignment\n";
+    ObjD1 = "Saya";
+    ObjD1.Show();
+    ObjD1.CBase::Show();
+    return 0;
+}
